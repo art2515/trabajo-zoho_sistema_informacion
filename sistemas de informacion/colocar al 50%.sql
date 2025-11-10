21810292cfe33826d679ad5b1e9cee51d40dbd69
CJVA <Cristianjva05@gmail.com>
2025-11-10 10:01:52 -0500

    Enhance payment process and add comments in SQL script
    Updated SQL script to automate the 50% payment process and added comments for clarity.

--- sistemas de informacion/colocar al 50%.sql
+++ sistemas de informacion/colocar al 50%.sql
@@ -1,8 +1,25 @@
-select tramite, orden, valor_financiacion, t.*, rowid from iceberg.cunt_tramite_externo t where identificacion = '1016949319';---1  
-    
-select t.*, rowid from iceberg.cltiene_transaccion_his t where numidentificacion= '1016949319';--- 2
+--- Aplicar el 50%
+--- El proceso es automatico , pero hay procesos que no pasa poque no esta cargado el pago de la rpvi , no tiene rpi , o no apto 
+--- Se revisa primero en iceber , que tenga RPVI y que la Fama no Este Liquidad
+--- el RPVI debe aparecer Liquidado , en tal caso de no estar se debe valida en Cartera de Iceber que tenga saldo a favor 
 
-select t.*, rowid from iceberg.cltiene_360_estudiantes t where numero_documento in ('1016949319');-----3     
+
+--- SE UTILIZA PARA BUSCAR LAS LIQUIDACIONES
+SELECT * FROM ICEBERG.LIQUIDACION_ORDEN WHERE orden = (110496);
+--- SE VEN TODOS LOS PROCESOS QUE A REALIZADO EL ESTUDIANTE
+select tramite, orden, valor_financiacion, t.*, rowid from iceberg.cunt_tramite_externo t where identificacion = '1012444362'; ---1  
+--- SE VE UN HISTORIAL DE TODO LO Q HA HECHO EL ESTUDIANTE    
+select t.*, rowid from iceberg.cltiene_transaccion_his t where numidentificacion= '1012444362'; --- 2
+--- SE VE EL ESTADO DE LA FINACIANCCION DEL ESTUDIANTE
+--- EL ESTADO DE FINANCIACION ES 0 50% , 1% 100% Y 2 ES ANULADO
+select t.*, rowid from iceberg.cltiene_360_estudiantes t where numero_documento in ('1012444362');-----3    
+
+
+---------------------COLOCAR_50%---------------------------------------------------------------------------------------------------------------------------------------------
+--- PARA INSERTAR LE PAGO A LA TABLA Y QUE LE APAREZCA AL 50%
+INSERT INTO ICEBERG.cltiene_360_estudiantes SELECT * FROM ICEBERG.v_inserta_estudiantes_360  WHERE numero_documento = '1012444362' AND referencia_pago = '118399658';
+
+ 
 -------------------CONSECUTIVO_RPVI------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE ICEBERG.ORDEN o SET o.CONSECUTIVO_ACTIVACION = (
@@ -12,6 +29,3 @@ WHERE o.PERIODO LIKE '%25%' AND o.ESTADO = 'V' AND o.DOCUMENTO = 'RPVI' AND o.CO
 
 SELECT O.*, ROWID FROM ICEBERG.ORDEN O WHERE O.CLIENTE_SOLICITADO = '1016949319' AND O.DOCUMENTO = 'RPVI'
 
----------------------COLOCAR_50%---------------------------------------------------------------------------------------------------------------------------------------------
-
-INSERT INTO ICEBERG.cltiene_360_estudiantes SELECT * FROM ICEBERG.v_inserta_estudiantes_360  WHERE numero_documento = '1016949319' AND referencia_pago = '117800612' 
