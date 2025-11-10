fd1c0368de17a973fae539f7a9d25a634688a402
CJVA <Cristianjva05@gmail.com>
2025-11-10 10:07:36 -0500

    Update comments and SQL logic for 50% application

--- sistemas de informacion/colocar al 50%.sql
+++ sistemas de informacion/colocar al 50%.sql
@@ -12,7 +12,9 @@ select tramite, orden, valor_financiacion, t.*, rowid from iceberg.cunt_tramite_
 select t.*, rowid from iceberg.cltiene_transaccion_his t where numidentificacion= '1012444362'; --- 2
 --- SE VE EL ESTADO DE LA FINACIANCCION DEL ESTUDIANTE
 --- EL ESTADO DE FINANCIACION ES 0 50% , 1% 100% Y 2 ES ANULADO
-select t.*, rowid from iceberg.cltiene_360_estudiantes t where numero_documento in ('1012444362');-----3    
+--- ESTA TABLA TAMBIEN SE UTILIZA PARA ANULAR UN 50% , EN ESE CASO SE QUITA EN ESTADO DE FINANCIACION COLOCANDO EL NUMERO EN 2 , ESO EN LAS DOS CULNAS , SE LLAMA IGUAL
+select referencia_pago,orden_cun,valor_financiacion,ESTADO_FINANCIACION, t.*
+from iceberg.cltiene_360_estudiantes t where numero_documento IN ('1012444362','');-----3    
 
 
 ---------------------COLOCAR_50%---------------------------------------------------------------------------------------------------------------------------------------------
@@ -28,4 +30,3 @@ WHERE o.PERIODO LIKE '%25%' AND o.ESTADO = 'V' AND o.DOCUMENTO = 'RPVI' AND o.CO
 (SELECT 1 FROM ICEBERG.LIQUIDACION_ORDEN l WHERE o.ORDEN = l.ORDEN AND o.PERIODO = l.PERIODO AND o.ESTADO = l.ESTADO AND o.DOCUMENTO = l.DOCUMENTO AND o.CLIENTE_SOLICITADO = l.CLIENTE);
 
 SELECT O.*, ROWID FROM ICEBERG.ORDEN O WHERE O.CLIENTE_SOLICITADO = '1016949319' AND O.DOCUMENTO = 'RPVI'
-
